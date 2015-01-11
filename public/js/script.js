1516ec68c9f32a1d9f636898b612f4e209f09c99
Martin Sookael <m@451.ee>
2015-01-11 16:22:40 +0200

    some design changes

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
   // connect-livereload via Gulp autorefreshes the site.
-  //$("body").append('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
+  $("body").append('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
 
   // hide loadin + show app
   $("#loading").addClass("animated fadeOut").addClass("hidden");
@@ -11,16 +11,20 @@ $(document).ready(function() {
   // VIEWS
   // Front page view
   var frontView = function () {
-    $("#otherPage, #admin").out();
-    $("#frontPage").in();
+    otherPage.out();
+    admin.out();
+    //$("#otherPage, #admin").out(); // For longer lists
+    frontPage.in();
     l("Front page view shown");
     frontPageFunction();
   }
 
   // Other page view
   var otherView = function () {
-    $("#frontPage, #admin").out("");
-    $("#otherPage").in("fadeInRight");
+    frontPage.out();
+    admin.out();
+
+    otherPage.in("bounce");
     l("Other page view shown");
     otherPageFunction();
   }
