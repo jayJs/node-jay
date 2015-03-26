9c957383f1f022be5efaf0a6e706d46b3832176a
Martin Sookael <m@451.ee>
2015-03-26 19:31:52 +0200

    hide livereload from master

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
 
   // connect-livereload via Gulp autorefreshes the site.
-  $("body").append('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
+  //$("body").append('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
 
   // hide loadin + show app
   $("#loading").out()
