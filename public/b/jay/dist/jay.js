af84b4c5e307bf35230574cf57fd7e3c78d0c9d2
Martin Sookael <m@451.ee>
2015-04-15 17:07:23 +0200

    initial new place for the alert

--- public/b/jay/dist/jay.js
+++ public/b/jay/dist/jay.js
@@ -120,7 +120,7 @@ function a(message) {
     elemParent.removeChild("alert");
   }
   // create the alert HTML
-  var extra = '<div id="alert" style="z-index: 10;"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><div id="alertMessage" class="alert alert-danger alert-dismissible" role="alert">'+message+'</div></div>';
+  var extra = '<div id="alert" style="z-index: 10;"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><div id="alertMessage" class="alert alert-black alert-dismissible" role="alert">'+message+'</div></div>';
   // add html to beginning of app
   var app = document.getElementById('app');
   app.innerHTML = extra + app.innerHTML;
