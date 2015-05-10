e8c5ba26acf7e2dc8e7ad6e74a280f27ab07aafc
Martin Sookael <m@451.ee>
2015-05-11 00:49:28 +0300

    initial minify + uglify works

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,17 @@ var lr;
 
 var EXPRESS_ROOT = __dirname;
 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
+/*
+gulp.task('scripts', function() {
+  gulp.src(['./public/b/jay/dist/jay.js', './public/b/momentjs/min/moment.min.js'])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+});
+*/
 function startLivereload() {
   lr = require('tiny-lr')();
   lr.listen(35729);
@@ -24,4 +34,19 @@ function notifyLivereload(event) {
 gulp.task('default', function () {
   startLivereload();
   gulp.watch('./public/**/*.*', notifyLivereload);
+
+  gulp.src([
+    './public/b/signals/dist/signals.min.js',
+    './public/b/hasher/dist/js/hasher.min.js',
+    './public/b/crossroads/dist/crossroads.min.js',
+    './public/b/html5shiv/dist/html5shiv.min.js',
+    './public/b/respond/dest/respond.min.js',
+    './public/b/bootstrap/dist/js/bootstrap.min.js',
+    './public/b/momentjs/min/moment.min.js',
+    './public/b/jay/dist/jay.js'
+    ])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/dist/'));
+
 });
