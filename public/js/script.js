612dd04e31b89bb7c481fbda6612326df77842e5
Martin Sookael <m@451.ee>
2015-06-02 18:28:17 +0300

    updated to new API

--- public/js/script.js
+++ public/js/script.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
   $("body").append('<script src="http://localhost:35729/livereload.js?snipver=1"></script>');
 
   // hide loadin + show app
-  $("#loading").out()
-  $("#app").in("fadeIn")
+  $("#loading").hide()
+  $("#app").show("fadeIn")
 
   function clearApp(){
-    addPost.out()
-    onePost.out()
-    onePost.empty()
-    listPosts.out()
-    logIn.out()
-    e404.out()
+    $(addPost).hide()
+    $(onePost).hide()
+    $(onePost).empty()
+    $(listPosts).hide()
+    $(logIn).hide()
+    $(e404).hide()
 
     $('html,body').scrollTop(0)
   }
@@ -24,7 +24,7 @@ $(document).ready(function() {
   // Front page view
   var listPostsView = function () {
     clearApp()
-    listPosts.in('fadeIn')
+    $(listPosts).show('fadeIn')
     listPostsFunction()
   }
 
@@ -32,7 +32,7 @@ $(document).ready(function() {
     isUser(function(){ // is a user
       clearApp()
       resetForm("addPostForm")
-      addPost.in('fadeIn');
+      $(addPost).show('fadeIn');
       addPostFunction();
     }, function() { // is not a user
       window.location = "#/login";
@@ -41,14 +41,14 @@ $(document).ready(function() {
 
   var onePostView = function (id) {
     clearApp()
-    onePost.in('fadeIn');
+    $(onePost).show('fadeIn');
     onePostFunction(id);
   }
 
   var editPostView = function (id) {
     isUser(function(){ // is a user
       clearApp()
-      addPost.in('fadeIn');
+      $(addPost).show('fadeIn');
       editPostFunction(id);
     }, function() { // is not a user
       window.location = "#/login";
@@ -58,7 +58,7 @@ $(document).ready(function() {
 
   var logInView = function() {
     clearApp()
-    logIn.in();
+    $(logIn).show();
     logInFunction()
   }
 
@@ -73,7 +73,7 @@ $(document).ready(function() {
   // that's a 404 if the route structure is not matched
   crossroads.bypassed.add(function(request){
     clearApp()
-    e404.in()
+    $(e404).show()
   })
 
   // start routing
@@ -84,11 +84,11 @@ $(document).ready(function() {
   function listPostsFunction() {
     get("Posts", 20).then(function(data){
       if(data.error === "No such post") {
-        e404.in()
+        $(e404).show()
       } else {
-        listPosts.empty();
+        $(listPosts).empty();
         for (var i = 0; i< data.length; i++) {
-          listPosts.append('<h3><a href="#/p/'+data[i].objectId+'">'+data[i].title+'</a></h3>')
+          $(listPosts).append('<h3><a href="#/p/'+data[i].objectId+'">'+data[i].title+'</a></h3>')
         }
       }
     })
@@ -106,20 +106,20 @@ $(document).ready(function() {
     var canUploadFiles = detectFileUpload();
     if(canUploadFiles === false) {
       a("This browser does not support file uploads");
-      image.parent().out()
+      image.parent().hide()
     }
 
     // create the preview for image
     $('#image').change(function(){
       var blob = getBlobURL($(this));
       if(blob != false) {
-        imagePreview.css("background-image", "url("+blob+")")
+        $(imagePreview).css("background-image", "url("+blob+")")
       }
     });
 
     // reset the form
     resetForm("addPostForm");
-    imagePreview.css("background-image", "")
+    $(imagePreview).css("background-image", "")
   }
 
   // Controller, "/add"
@@ -136,7 +136,7 @@ $(document).ready(function() {
       var d = data[0];
       rebuildForm("addPostForm", d);
       // rebuildForm() does not take input file yet, so:
-      if(d.image && d.image.url) { imagePreview.css("background-image", "url("+d.image.url+")"); }
+      if(d.image && d.image.url) { $(imagePreview).css("background-image", "url("+d.image.url+")"); }
     })
     saveForm("Posts", 'addPostForm', id);
   }
@@ -162,27 +162,27 @@ $(document).ready(function() {
     get("Posts", 1,  id).then(function(data_0) {
       var data = data_0[0]
       if(data_0.error === "No such post" || data_0.error === "Query is confused") {
-        e404.in()
+        $(e404).show()
       } else {
         if(typeof data.titles === "string") {
           data.titles = JSON.parse(data.titles);
         }
-        onePost.empty();
+        $(onePost).empty();
         $.each(data, function(key, value) {
           if(key == "updatedAt" || key == "createdAt" || key == "objectId") {}
           else {
             if(value.length>0) {
-              onePost.append("<h4>"+data.titles[key]+"<h4>");
-              onePost.append(value);
+              $(onePost).append("<h4>"+data.titles[key]+"<h4>");
+              $(onePost).append(value);
             }
             // if it's a parse url
             if(value.url) {
-              onePost.append("<h4>"+data.titles[key]+"<h4>");
-              onePost.append("<img src='"+value.url+"' style='width: 300px; height: auto;'>");
+              $(onePost).append("<h4>"+data.titles[key]+"<h4>");
+              $(onePost).append("<img src='"+value.url+"' style='width: 300px; height: auto;'>");
             }
           }
         })
-        onePost.append("<br /><br /><br /><a href='#/e/"+id+"'>Edit this post</a>")
+        $(onePost).append("<br /><br /><br /><a href='#/e/"+id+"'>Edit this post</a>")
 
       }
     });
